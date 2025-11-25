37dd0be75b07663d367ae298c15a3efad8dacf90
geraskinalekseysergeevich <leha.geraskin.80@gmail.com>
2025-11-26 05:27:34 +0800

    feat: add card form and refactor code

--- src/components/card.js
+++ src/components/card.js
@@ -1,11 +1,5 @@
-import { initialCards } from "./cards"
-import { openImagePopup } from "./modal"
-
-const placesList = document.querySelector(".places__list")
-const cardTemplate = document.querySelector("#card-template").content
-
-export function createCard(name, link) {
-	const card = cardTemplate.querySelector(".card").cloneNode(true)
+export function createCard(template, name, link, handleImageClick) {
+	const card = template.querySelector(".card").cloneNode(true)
 
 	const image = card.querySelector(".card__image")
 	const title = card.querySelector(".card__title")
@@ -15,18 +9,8 @@ export function createCard(name, link) {
 	image.alt = name
 	title.textContent = name
 
-	deleteButton.addEventListener("click", () => {
-		card.remove()
-	})
-
-	image.addEventListener("click", () => {
-		openImagePopup(name, link)
-	})
+	deleteButton.addEventListener("click", () => card.remove())
+	image.addEventListener("click", () => handleImageClick(name, link))
 
 	return card
 }
-
-initialCards.forEach(({ name, link }) => {
-	const cardElement = createCard(name, link)
-	placesList.append(cardElement)
-})
--- src/components/modal.js
+++ src/components/modal.js
@@ -1,80 +1,29 @@
-const popupEdit = document.querySelector(".popup_type_edit")
-const popupAddCard = document.querySelector(".popup_type_new-card")
-const popups = document.querySelectorAll(".popup")
-
-const editButton = document.querySelector(".profile__edit-button")
-const addButton = document.querySelector(".profile__add-button")
-const closeButtons = document.querySelectorAll(".popup__close")
-
-const profileName = document.querySelector(".profile__title")
-const profileDescription = document.querySelector(".profile__description")
-
-const editProfileForm = document.querySelector('.popup__form[name="edit-profile"]')
-const profileNameInput = editProfileForm.querySelector(".popup__input_type_name")
-const profileJobInput = editProfileForm.querySelector(".popup__input_type_description")
-
-function openPopup(popup) {
+export function openPopup(popup) {
 	popup.classList.add("popup_is-opened")
 	document.addEventListener("keydown", handleEscClose)
 }
 
-function closePopup(popup) {
+export function closePopup(popup) {
 	popup.classList.remove("popup_is-opened")
 	document.removeEventListener("keydown", handleEscClose)
 }
 
-// ESC close
-function handleEscClose(evt) {
+export function handleEscClose(evt) {
 	if (evt.key === "Escape") {
 		const openedPopup = document.querySelector(".popup_is-opened")
 		if (openedPopup) closePopup(openedPopup)
 	}
 }
 
-// overlay close
-function handleOverlayClose(evt) {
+export function handleOverlayClose(evt) {
 	if (evt.target.classList.contains("popup")) {
 		closePopup(evt.target)
 	}
 }
 
-popups.forEach(popup => {
-	popup.addEventListener("mousedown", handleOverlayClose)
-})
-
-addButton.addEventListener("click", () => openPopup(popupAddCard))
-editButton.addEventListener("click", () => {
-	profileNameInput.value = profileName.textContent
-	profileJobInput.value = profileDescription.textContent
-	openPopup(popupEdit)
-})
-
-closeButtons.forEach(closeButton => {
-	closeButton.addEventListener("click", () => closePopup(closeButton.closest(".popup")))
-})
-
-function handleFormSubmit(evt) {
-	evt.preventDefault()
-
-	const newName = profileNameInput.value
-	const newJob = profileJobInput.value
-
-	profileName.textContent = newName
-	profileDescription.textContent = newJob
-
-	closePopup(popupEdit)
-}
-
-editProfileForm.addEventListener("submit", handleFormSubmit)
-
-// image popup
-const popupImage = document.querySelector(".popup_type_image")
-const popupImageImg = popupImage.querySelector(".popup__image")
-const popupImageCaption = popupImage.querySelector(".popup__caption")
-
-export function openImagePopup(name, link) {
-	popupImageImg.src = link
-	popupImageImg.alt = name
-	popupImageCaption.textContent = name
-	openPopup(popupImage)
+export function openImagePopup(popup, imageElement, captionElement, name, link) {
+	imageElement.src = link
+	imageElement.alt = name
+	captionElement.textContent = name
+	openPopup(popup)
 }
