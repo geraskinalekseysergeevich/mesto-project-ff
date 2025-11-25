98e732a9c7208022f9bcf1c128c9535c4ce5ec5b
geraskinalekseysergeevich <leha.geraskin.80@gmail.com>
2025-11-26 03:40:50 +0800

    feat: init cards and createCard func

---
+++ src/components/card.js
@@ -0,0 +1,27 @@
+import { initialCards } from "./cards"
+
+const placesList = document.querySelector(".places__list")
+const cardTemplate = document.querySelector("#card-template").content
+
+export function createCard(name, link) {
+	const card = cardTemplate.querySelector(".card").cloneNode(true)
+
+	const image = card.querySelector(".card__image")
+	const title = card.querySelector(".card__title")
+	const deleteButton = card.querySelector(".card__delete-button")
+
+	image.src = link
+	image.alt = name
+	title.textContent = name
+
+	deleteButton.addEventListener("click", () => {
+		card.remove()
+	})
+
+	return card
+}
+
+initialCards.forEach(({ name, link }) => {
+	const cardElement = createCard(name, link)
+	placesList.append(cardElement)
+})
