00aea4948a12641241d2181b73f911566848e76d
geraskinalekseysergeevich <leha.geraskin.80@gmail.com>
2025-11-26 04:48:26 +0800

    feat: edit profile form actions

--- src/components/modal.js
+++ src/components/modal.js
@@ -4,8 +4,14 @@ const popups = document.querySelectorAll(".popup")
 
 const editButton = document.querySelector(".profile__edit-button")
 const addButton = document.querySelector(".profile__add-button")
-
 const closeButtons = document.querySelectorAll(".popup__close")
+
+const profileName = document.querySelector(".profile__title")
+const profileDescription = document.querySelector(".profile__description")
+
+const editProfileForm = document.querySelector('.popup__form[name="edit-profile"]')
+const profileNameInput = editProfileForm.querySelector(".popup__input_type_name")
+const profileJobInput = editProfileForm.querySelector(".popup__input_type_description")
 
 function openPopup(popup) {
 	popup.classList.add("popup_is-opened")
@@ -37,11 +43,29 @@ popups.forEach(popup => {
 })
 
 addButton.addEventListener("click", () => openPopup(popupAddCard))
-editButton.addEventListener("click", () => openPopup(popupEdit))
+editButton.addEventListener("click", () => {
+	profileNameInput.value = profileName.textContent
+	profileJobInput.value = profileDescription.textContent
+	openPopup(popupEdit)
+})
 
 closeButtons.forEach(closeButton => {
 	closeButton.addEventListener("click", () => closePopup(closeButton.closest(".popup")))
 })
+
+function handleFormSubmit(evt) {
+	evt.preventDefault()
+
+	const newName = profileNameInput.value
+	const newJob = profileJobInput.value
+
+	profileName.textContent = newName
+	profileDescription.textContent = newJob
+
+	closePopup(popupEdit)
+}
+
+editProfileForm.addEventListener("submit", handleFormSubmit)
 
 // image popup
 const popupImage = document.querySelector(".popup_type_image")
