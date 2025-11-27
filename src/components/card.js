91f0117ef868bd06c1b0bb919146fbaa68db5bb3
geraskinalekseysergeevich <leha.geraskin.80@gmail.com>
2025-11-28 02:02:35 +0800

    feat: likes

--- src/components/card.js
+++ src/components/card.js
@@ -1,16 +1,26 @@
-export function createCard(template, name, link, handleImageClick) {
+export function toggleLike(evt) {
+	evt.target.classList.toggle("card__like-button_is-active")
+}
+
+export function removeCard(cardElement) {
+	cardElement.remove()
+}
+
+export function createCard(template, name, link, openImagePopup) {
 	const card = template.querySelector(".card").cloneNode(true)
 
 	const image = card.querySelector(".card__image")
 	const title = card.querySelector(".card__title")
 	const deleteButton = card.querySelector(".card__delete-button")
+	const likeButton = card.querySelector(".card__like-button")
 
 	image.src = link
 	image.alt = name
 	title.textContent = name
 
-	deleteButton.addEventListener("click", () => card.remove())
-	image.addEventListener("click", () => handleImageClick(name, link))
+	deleteButton.addEventListener("click", () => removeCard(card))
+	image.addEventListener("click", () => openImagePopup(name, link))
+	likeButton.addEventListener("click", toggleLike)
 
 	return card
 }
